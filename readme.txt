3f67ba2b0d028227346e514ccd7adbe1f01f3c32
wpdennis <wpdennis@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-09-27 11:18:36 +0000

    * Moved to WordPress Repository. * Changed the ReadMe.
    git-svn-id: https://plugins.svn.wordpress.org/standout-content/trunk@444155 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -3,8 +3,8 @@ Contributors: wpdennis
 Plugin Name: Standout Content
 Plugin URI: http://www.gameplorer.de/tools/standout-content/
 Author: Dennis Pietsch
-Author URI: http://www.gameplorer.de/
-Tags: standout, google, news
+Author URI: http://www.dennispietsch.com
+Tags: standout, google, news, tag, link, rel, content
 Requires at least: 3.0
 Tested up to: 3.2.1
 Stable tag: trunk
@@ -13,22 +13,23 @@ Standout Content adds an option to mark a post as standout for Google News.
 
 == Description ==
 
-Google announced the new standout tag for Google News publishers. If you put the tag in the HTML header of one of your articles, Google News may show the article with a ‘Featured’ label on the Google News homepage and News Search results. Please see the official post:
-<a href="http://googlenewsblog.blogspot.com/2011/09/recognizing-publishers-standout-content.html">http://googlenewsblog.blogspot.com/</a>
+This plugin adds an option to mark a post as standout for Google News, the new link tag introduced on September 24, 2011 by Google. You can check or uncheck the option for each post. 
 
-This plugin adds a simple checkbox to each post where you can define if it stands out. 
+You souldn't mark too many posts as standout (or your tags get less recognized by Google). To help you with that, this plugin shows you each post tagged within the last 7 days.
+
+This plugin was developed for our <a href="http://www.gameplorer.de">Casual Games Blog</a>. I hope it will help you too.
+For further information about standout content, please see the official post:
+<a href="http://googlenewsblog.blogspot.com/2011/09/recognizing-publishers-standout-content.html">http://googlenewsblog.blogspot.com2011/09/recognizing-publishers-standout-content.html</a>
 
 == Screenshots ==
 
 1. Just check or uncheck the checkbox below your post editor.
 
-== Installation ==
-
-1. Upload the whole folder to the `/wp-content/plugins/` directory.
-2. Activate the plugin through the 'Plugins' menu in WordPress.
-3. You're done. Edit your posts and check the standout-checkbox.
-
 == Changelog ==
 
-V1.0, 09/26/2011
-Initial upload.
+= v1.1, 09/27/2011 =
+* Moved to WordPress Repository.
+* Changed the ReadMe.
+
+= v1.0, 09/26/2011 =
+* Initial version.
--- standout-content.php
+++ standout-content.php
@@ -1,98 +1,102 @@
 <?php
 /*
 Plugin Name: Standout Content
-Plugin URI: http://www.gameplorer.de
-Description: Standout Content adds an option to mark a post as standout for Google News. You can check or uncheck the option in each post.
+Plugin URI: http://www.gameplorer.de/tools/standout-content/
+Description: Standout Content adds an option to mark a post as standout for Google News. You can check or uncheck the option for each post.
 Author: Dennis Pietsch
-Author URI: http://www.gameplorer.de
-Tags: standout, google, news
-Version: 1.0
+Author URI: http://www.dennispietsch.com
+Tags: standout, google, news, tag, link, rel, content
+Version: 1.1
 */
 
-class StandoutContent {
+if (!class_exists('StandoutContent')):
 
-	private $metaKey = '_standout_content';
+	class StandoutContent {
 
-	public function __construct() {
-		add_action('admin_menu', array(&$this, 'onAdminMenu'));
-		add_action('save_post', array(&$this, 'onSave'));
-		add_action('wp_head', array(&$this, 'onWPHead'));
-	}
-	
-	public function onAdminMenu() {
-		add_meta_box('standout-content-metabox', 'Standout Content', array(&$this, 'metaBox'), 'post');
-	}
-		
-	public function onWPHead() {
-	
-		if (!is_single())
-			return;
-			
-		global $post;
-		$postId = (is_object($post)) ? $post->ID : $post;
-		
-		$isStandoutContent = get_post_meta($postId, $this->metaKey, true);
-		if ($isStandoutContent == '1')
-			echo '<link rel="standout" href="' . get_permalink($postId) . '" />' . "\n";
-	}
-		
-	public function onSave($postId) {
-	
-		// verify nonce
-		if (!wp_verify_nonce($_POST['standout_content_noncename'], plugin_basename(__FILE__)))
-			return $postId;
-		
-		// never save while auto saving
-		if (defined('DOING_AUTOSAVE') and DOING_AUTOSAVE) 
-			return $postId;
-			
-		// check permission
-		if (!current_user_can('edit_post', $postId))
-			return $postId;
-			
-		// save
-		if (isset($_POST['standout_content_checkbox']) and $_POST['standout_content_checkbox'] == "1")
-			update_post_meta($postId, $this->metaKey, '1');
-		else
-			delete_post_meta($postId, $this->metaKey);
-	}
-	
-	public function metaBox() {
-	
-		global $post;
-		$postId = (is_object($post)) ? $post->ID : $post;
-	
-		echo '<input type="hidden" name="standout_content_noncename" value="' . wp_create_nonce(plugin_basename(__FILE__)) . '" />';
-		$checked = get_post_meta($postId, $this->metaKey, true) == '1' ? ' checked="checked"' : null;
-		
-		echo '<p><input type="checkbox" name="standout_content_checkbox" id="standout_content_checkbox"' . $checked . ' value="1" /> Add a <a href="http://googlenewsblog.blogspot.com/2011/09/recognizing-publishers-standout-content.html" target="_blank">standout tag</a> to this post.</p>';
-		
-		$posts = $this->getRecentStandoutPosts();
-		echo '<p>You tagged <strong>' . count($posts) . '</strong> post(s) as Standout Content within the last 7 days.';
-		if (count($posts) > 0):
-			$postsStream = '<br/>';
-			foreach ($posts as $p):
-				$postsStream .= '<a href="' . get_bloginfo('url') . '/wp-admin/post.php?post=' . $p->ID . '&action=edit">' . $p->post_title . '</a> | ';
-			endforeach;
-			$postsStream = substr($postsStream, 0, strlen($postsStream) - 2);
-			echo $postsStream;
-		endif;
-		echo '</p>';
-	}
-	
-	private function getRecentStandoutPosts() {
-		global $wpdb;
-		$qry = 'SELECT p.ID, p.post_title
-				FROM ' . $wpdb->posts . ' AS p
-					INNER JOIN ' . $wpdb->postmeta . ' AS m ON p.ID = m.post_id AND m.meta_key = "' . $this->metaKey . '"
-				WHERE p.post_status = "publish"
-				  AND m.meta_value = "1"
-				  AND DATEDIFF(NOW(), p.post_date) < 8
-				ORDER BY p.post_date DESC';
-		return $wpdb->get_results($qry);
-	}
-}
+		private $metaKey = '_standout_content';
 
-// initialize
-new StandoutContent();
+		public function __construct() {
+			add_action('admin_menu', array(&$this, 'onAdminMenu'));
+			add_action('save_post', array(&$this, 'onSave'));
+			add_action('wp_head', array(&$this, 'onWPHead'));
+		}
+		
+		public function onAdminMenu() {
+			add_meta_box('standout-content-metabox', 'Standout Content', array(&$this, 'metaBox'), 'post');
+		}
+			
+		public function onWPHead() {
+		
+			if (!is_single())
+				return;
+				
+			global $post;
+			$postId = (is_object($post)) ? $post->ID : $post;
+			
+			$isStandoutContent = get_post_meta($postId, $this->metaKey, true);
+			if ($isStandoutContent == '1')
+				echo '<link rel="standout" href="' . get_permalink($postId) . '" />' . "\n";
+		}
+			
+		public function onSave($postId) {
+		
+			// verify nonce
+			if (!wp_verify_nonce($_POST['standout_content_noncename'], plugin_basename(__FILE__)))
+				return $postId;
+			
+			// never save while auto saving
+			if (defined('DOING_AUTOSAVE') and DOING_AUTOSAVE) 
+				return $postId;
+				
+			// check permission
+			if (!current_user_can('edit_post', $postId))
+				return $postId;
+				
+			// save
+			if (isset($_POST['standout_content_checkbox']) and $_POST['standout_content_checkbox'] == "1")
+				update_post_meta($postId, $this->metaKey, '1');
+			else
+				delete_post_meta($postId, $this->metaKey);
+		}
+		
+		public function metaBox() {
+		
+			global $post;
+			$postId = (is_object($post)) ? $post->ID : $post;
+		
+			echo '<input type="hidden" name="standout_content_noncename" value="' . wp_create_nonce(plugin_basename(__FILE__)) . '" />';
+			$checked = get_post_meta($postId, $this->metaKey, true) == '1' ? ' checked="checked"' : null;
+			
+			echo '<p><input type="checkbox" name="standout_content_checkbox" id="standout_content_checkbox"' . $checked . ' value="1" /> Add a <a href="http://googlenewsblog.blogspot.com/2011/09/recognizing-publishers-standout-content.html" target="_blank">standout tag</a> to this post.</p>';
+			
+			$posts = $this->getRecentStandoutPosts();
+			echo '<p>You tagged <strong>' . count($posts) . '</strong> post(s) as Standout Content within the last 7 days.';
+			if (count($posts) > 0):
+				$postsStream = '<br/>';
+				foreach ($posts as $p):
+					$postsStream .= '<a href="' . get_bloginfo('url') . '/wp-admin/post.php?post=' . $p->ID . '&action=edit">' . $p->post_title . '</a> | ';
+				endforeach;
+				$postsStream = substr($postsStream, 0, strlen($postsStream) - 2);
+				echo $postsStream;
+			endif;
+			echo '</p>';
+		}
+		
+		private function getRecentStandoutPosts() {
+			global $wpdb;
+			$qry = 'SELECT p.ID, p.post_title
+					FROM ' . $wpdb->posts . ' AS p
+						INNER JOIN ' . $wpdb->postmeta . ' AS m ON p.ID = m.post_id AND m.meta_key = "' . $this->metaKey . '"
+					WHERE p.post_status = "publish"
+					  AND m.meta_value = "1"
+					  AND DATEDIFF(NOW(), p.post_date) < 8
+					ORDER BY p.post_date DESC';
+			return $wpdb->get_results($qry);
+		}
+	}
+
+	// initialize
+	new StandoutContent();
+	
+endif;
 ?>
